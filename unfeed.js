fc010c0fb652ca387720010b9104cfdde28bb54a
Charles Lai <cjl223@cornell.edu>
2014-07-20 16:27:25 -0500

    make unfeed.js indentation 2 spaces

--- unfeed.js
+++ unfeed.js
@@ -1,7 +1,7 @@
 /*
-	unfeed.js
-	Removes key profile information from the newsfeed
-	@author: Charles J Lai
+  unfeed.js
+  Removes key profile information from the newsfeed
+  @author: Charles J Lai
 */
 
 //Constants
@@ -10,34 +10,34 @@ var LONG_UPDATE_INTERVAL = 10000;
 
 //Main profile information removal function
 function removeProfileInfo() {
-	//Get arrays of nodes
-	var profileNames = document.querySelectorAll(".fwb, .profileLink");
-	if (typeof profileNames === undefined) {
-		return;
-	}
-	var profilePics = document.getElementsByClassName("_s0 _5xib _5sq7 _rw img");
-	var commentPics = document.getElementsByClassName("UFIImageBlockImage");
-	var commentNames = document.getElementsByClassName("UFICommentActorName");
+  //Get arrays of nodes
+  var profileNames = document.querySelectorAll(".fwb, .profileLink");
+  if (typeof profileNames === undefined) {
+    return;
+  }
+  var profilePics = document.getElementsByClassName("_s0 _5xib _5sq7 _rw img");
+  var commentPics = document.getElementsByClassName("UFIImageBlockImage");
+  var commentNames = document.getElementsByClassName("UFICommentActorName");
 
-	//Remove all profile pictures in the viewport
-	for (var i = 0; i < profilePics.length; i++) {
-		profilePics[i].remove();
-	}
+  //Remove all profile pictures in the viewport
+  for (var i = 0; i < profilePics.length; i++) {
+    profilePics[i].remove();
+  }
 
-	//Remove all commenter pictures in the viewport
-	for (var i = 0; i < commentPics.length; i++) {
-		commentPics[i].remove();
-	}
+  //Remove all commenter pictures in the viewport
+  for (var i = 0; i < commentPics.length; i++) {
+    commentPics[i].remove();
+  }
 
-	//Replace all profile names in the view port with "Someone".
-	for (var i = 0; i < profileNames.length; i++) {
-		profileNames[i].innerHTML = "Someone";
-	}
+  //Replace all profile names in the view port with "Someone".
+  for (var i = 0; i < profileNames.length; i++) {
+    profileNames[i].innerHTML = "Someone";
+  }
 
-	//Replace all commenter names in the view port with "Someone".
-	for (var i = 0; i < commentNames.length; i++) {
-		commentNames[i].innerHTML = "Someone";
-	}
+  //Replace all commenter names in the view port with "Someone".
+  for (var i = 0; i < commentNames.length; i++) {
+    commentNames[i].innerHTML = "Someone";
+  }
 }
 
 ///////////////
